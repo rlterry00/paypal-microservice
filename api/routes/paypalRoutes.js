a6c9eb0ff432608bb3cbf4036930be5951075034
rlterry00 <chuckterry00@gmail.com>
2021-09-01 14:44:18 -0400

    added https and ssl cert

--- api/routes/paypalRoutes.js
+++ api/routes/paypalRoutes.js
@@ -4,10 +4,41 @@ const paypalTokenURL = process.env.PAYPAL_TOKEN_URL;
 const clientId = process.env.SANDBOX_CLIENT_ID;
 const secret = process.env.SANDBOX_SECRET;
 
-
 module.exports = function (app) {
   app.post("/pennybankplus/subscribed", (req, res, next) => {
-      res.send(req.body);
-      
+    res.send(req.body);
+    const getToken = async () => {
+      const params = new URLSearchParams();
+      params.append("grant_type", "client_credentials");
+      axios
+        .post(
+          paypalTokenURL,
+          {
+            headers: {
+              Accept: "application/json",
+              "Accept-Language": "en_US",
+              "content-type": "application/x-www-form-urlencoded",
+            },
+          },
+          {
+            auth: {
+              username: clientId,
+              password: secret,
+            },
+          },
+          {
+            params: {
+              grant_type: "client_credentials",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    };
+    getToken();
   });
 };
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
+const fs = require("fs");
+const key = fs.readFileSync("./key.pem");
+const cert = fs.readFileSync("./cert.pem");
 var express = require("express");
+const https = require("https");
 var app = express();
 port = process.env.PORT || 3000;
 
@@ -10,10 +14,12 @@ app.use(bodyParser.json());
 var routes = require("./api/routes/paypalRoutes");
 routes(app);
 
+const server = https.createServer({ key: key, cert: cert }, app);
+
+server.listen(port, () => {
+  console.log("listening on " + port);
+});
+
 app.use(function (req, res) {
   res.status(404).send(req.originalUrl + " 404 error not found");
 });
-
-app.listen(port);
-
-console.log("API server started on: " + port);
