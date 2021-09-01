aff49d92636c25afe760c74ab582c7cb7373a551
rlterry00 <chuckterry00@gmail.com>
2021-09-01 08:22:22 -0400

    changed index to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 port = process.env.PORT || 3000;
+bodyParser = require('body-parser');
 
 app.get("/url", (req, res, next) => {
  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
