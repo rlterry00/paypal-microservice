be4e1f634a3a0670d02323f7da4b9669485f6c0d
rlterry00 <chuckterry00@gmail.com>
2021-09-01 13:20:11 -0400

    added .env varibles

--- api/routes/paypalRoutes.js
+++ api/routes/paypalRoutes.js
@@ -1,10 +1,13 @@
 "use strict";
 const axios = require("axios").default;
-
+const paypalTokenURL = process.env.PAYPAL_TOKEN_URL;
+const clientId = process.env.SANDBOX_CLIENT_ID;
+const secret = process.env.SANDBOX_SECRET;
 
 
 module.exports = function (app) {
   app.post("/pennybankplus/subscribed", (req, res, next) => {
-    res.send(req.body);
+      res.send(req.body);
+      
   });
 };
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
 port = process.env.PORT || 3000;
 
-bodyParser = require('body-parser');
+bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/paypalRoutes'); 
-routes(app); 
+var routes = require("./api/routes/paypalRoutes");
+routes(app);
 
 app.use(function (req, res) {
-  res.status(404).send( req.originalUrl + " 404 error not found" );
+  res.status(404).send(req.originalUrl + " 404 error not found");
 });
 
 app.listen(port);
 
-console.log('API server started on: ' + port);
+console.log("API server started on: " + port);
