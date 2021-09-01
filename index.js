da49a2e712a24ca26c9a724c0630e19f9d3f4db8
rlterry00 <chuckterry00@gmail.com>
2021-09-01 08:12:15 -0400

    added axios

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+port = process.env.PORT || 3000;
+
+app.get("/url", (req, res, next) => {
+ res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
