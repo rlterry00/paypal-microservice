2402f41df67b8fae927c13f78394ac3736312421
rlterry00 <chuckterry00@gmail.com>
2021-09-01 21:27:36 -0400

    added  gitignore and create and get subcriber id

--- api/routes/paypalRoutes.js
+++ api/routes/paypalRoutes.js
@@ -1,24 +1,22 @@
 "use strict";
 const axios = require("axios").default;
 const paypalTokenURL = process.env.PAYPAL_TOKEN_URL;
-const subscriptionURL =
-  "https://api.paypal.com/v1/billing/subscriptions/I-3SHRMPFTVBU4";
-const createSubscriptionURL = "https://api.paypal.com/v1/billing/subscriptions";
+const subscriptionURL = "https://api.paypal.com/v1/billing/subscriptions";
 const clientId = process.env.SANDBOX_CLIENT_ID;
 const secret = process.env.SANDBOX_SECRET;
 const planId = process.env.PLAN_ID;
 
 module.exports = function (app) {
-  app.post("/pennybankplus/create/subscription", (req, res, next) => {
-    res.send(req.body);
+  //Create a new subscription and get id
+  app.post("/api/pennybankplus/create/subscription", (req, res, next) => {
     const createSubsription = async () => {
       axios({
         method: "post",
-        url: "https://api.paypal.com/v1/oauth2/token",
-        data: "grant_type=client_credentials", // => this is mandatory x-www-form-urlencoded. DO NOT USE json format for this
+        url: paypalTokenURL,
+        data: "grant_type=client_credentials",
         headers: {
           Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded", // => needed to handle data parameter
+          "Content-Type": "application/x-www-form-urlencoded",
           "Accept-Language": "en_US",
         },
         auth: {
@@ -31,9 +29,9 @@ module.exports = function (app) {
           const token = response.data.access_token;
           axios
             .post(
-              createSubscriptionURL,
+              subscriptionURL,
               {
-                plan_id: planId
+                plan_id: planId,
               },
               {
                 headers: {
@@ -44,6 +42,11 @@ module.exports = function (app) {
             )
             .then((response) => {
               console.log(response.data);
+              res.send({
+                status: response.data.status,
+                subscriptionId: response.data.id,
+                link: response.data.links[0].href,
+              });
             })
             .catch((error) => {
               console.log(error.response);
@@ -54,5 +57,51 @@ module.exports = function (app) {
         });
     };
     createSubsription();
+  });
+
+  //Check status of a subscription by id
+  app.post("/api/pennybankplus/status", (req, res, next) => {
+    const subscriptionId = req.body.subscriptionId;
+    const getStatus = async () => {
+      axios({
+        method: "post",
+        url: paypalTokenURL,
+        data: "grant_type=client_credentials",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Accept-Language": "en_US",
+        },
+        auth: {
+          username: clientId,
+          password: secret,
+        },
+      })
+        .then((response) => {
+          console.log(response.data.access_token);
+          const token = response.data.access_token;
+          axios
+            .get(subscriptionURL + "/" + subscriptionId, {
+              headers: {
+                Accept: "application/json",
+                Authorization: "Bearer " + token,
+              },
+            })
+            .then((response) => {
+              console.log(response.data);
+              res.send({
+                status: response.data.status,
+                subscriptionId: response.data.id,
+              });
+            })
+            .catch((error) => {
+              console.log(error.response);
+            });
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    };
+    getStatus();
   });
 };
