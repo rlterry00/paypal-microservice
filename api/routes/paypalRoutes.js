70703f875d66016135c7362c74ca7a69b57f3888
rlterry00 <chuckterry00@gmail.com>
2021-09-05 08:28:39 -0400

    changed controller naming convention to camel case

--- api/routes/paypalRoutes.js
+++ api/routes/paypalRoutes.js
@@ -1,12 +1,12 @@
-var paypal_controller = require("../controllers/paypalControllers");
+var paypalControllers = require("../controllers/paypalControllers");
 
 module.exports = function (app) {
   //Create a new subscription and get id
-  app.post("/api/pennybankplus/create/subscription", paypal_controller.create);
+  app.post("/api/pennybankplus/create/subscription", paypalControllers.create);
 
   //Check status of a subscription by id
-  app.post("/api/pennybankplus/status", paypal_controller.status);
+  app.post("/api/pennybankplus/status", paypalControllers.status);
 
   //Cancel a subscription by id
-  app.post("/api/pennybankplus/cancel", paypal_controller.cancel);
+  app.post("/api/pennybankplus/cancel", paypalControllers.cancel);
 };
