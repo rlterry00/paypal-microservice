4ab909487df862500faf5d525357beeaaacdd6ca
rlterry00 <chuckterry00@gmail.com>
2021-09-17 06:29:02 -0400

    successfully able to check subscription status and patch pb backend

--- api/controllers/paypalControllers.js
+++ api/controllers/paypalControllers.js
@@ -12,11 +12,12 @@ const planId = process.env.PLAN_ID;
 const authSecret = process.env.AUTH_SECRET;
 const pbCred = process.env.PB_CRED;
 const pbAuth = process.env.PB_AUTH;
+const moment = require("moment");  
 
 //Create an auth JWT based on valid token from penny bank.
 exports.auth = (req, res, next) => {
   const token = req.headers.authorization;
-  const userId= req.headers.userid;
+  const userId = req.headers.userid;
   const getFamilyURL = checkFamilyURL + userId;
 
   axios
@@ -32,11 +33,19 @@ exports.auth = (req, res, next) => {
       const authToken = jwt.sign({ token: token, userId: userId }, authSecret, {
         expiresIn: 600,
       });
-      res.send({
-        status: 200,
-        authToken: authToken,
-        subscription: response.data.subscriberId
-      });
+      if (response.data.subscriberId) {
+        res.send({
+          status: 200,
+          authToken: authToken,
+          subscription: response.data.subscriberId,
+        });
+      } else {
+        res.send({
+          status: 404,
+          authToken: authToken,
+          message: "subscription id not found",
+        });
+      }
     })
     .catch((error) => {
       console.log(error);
@@ -44,7 +53,7 @@ exports.auth = (req, res, next) => {
       res.send({
         status: 404,
         authToken: authToken,
-        message: "subscription id not found"
+        message: "subscription id not found",
       });
     });
 };
@@ -146,6 +155,8 @@ exports.status = (req, res, next) => {
   } else {
     const authToken = req.headers.authorization;
     jwt.verify(authToken, authSecret, (err, decoded) => {
+      const pbToken = decoded.token;
+      const familyId = decoded.userId;
       if (!err) {
         const subscriptionId = req.body.subscriptionId;
         const getStatus = async () => {
@@ -175,7 +186,43 @@ exports.status = (req, res, next) => {
                 })
                 .then((response) => {
                   console.log(response.data);
-                  //will need to update family model with subscriptionId in db
+                  const nextBilling = moment
+                    .utc(response.data.billing_info.next_billing_time)
+                    .local()
+                    .format("YYYY-MM-DDTHH:mm:ss");
+                  const activeFrom = moment
+                    .utc(response.data.create_time)
+                    .local()
+                    .format("YYYY-MM-DDTHH:mm:ss");
+                  if (response.data.status !== "APPROVAL_PENDING") {
+                    axios
+                      .patch(
+                        updateSubscriberURL + familyId + "/update",
+                        {
+                          active:
+                            response.data.status == "ACTIVE" ? true : false,
+                          subscriberId: response.data.id,
+                          planId: response.data.plan_id,
+                          emailAddress: response.data.subscriber.email_address,
+                          // nextBillingTime: String(nextBilling),
+                          // activeFrom: String(activeFrom),
+                        },
+                        {
+                          headers: {
+                            Authorization: pbToken,
+                            "Subsvr-Creds": pbCred,
+                            "Subsvr-Auth": pbAuth,
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        console.log(response.data);
+                      })
+                      .catch((error) => {
+                        console.log(error.response);
+                      });
+                  }
+
                   res.send({
                     status: response.data.status,
                     subscriptionId: response.data.id,
@@ -183,17 +230,17 @@ exports.status = (req, res, next) => {
                 })
                 .catch((error) => {
                   console.log(error.response);
-                  if (error.response.status == 404) {
-                    res.send({
-                      status: error.response.status,
-                      error: "Subscription ID no longer valid",
-                    });
-                  } else {
-                    res.send({
-                      status: error.response.status,
-                      error: error.response.statusText,
-                    });
-                  }
+                  // if (error.response.status == 404) {
+                  //   res.send({
+                  //     status: error.response.status,
+                  //     error: "Subscription ID no longer valid",
+                  //   });
+                  // } else {
+                  //   res.send({
+                  //     status: error.response.status,
+                  //     error: error.response.statusText,
+                  //   });
+                  // }
                 });
             })
             .catch((error) => {
