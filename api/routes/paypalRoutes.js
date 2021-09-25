224d4bc3ac33b1a3c0527d472228f7a4bb71d8a4
rlterry00 <chuckterry00@gmail.com>
2021-09-25 01:13:40 -0400

    added /sub to path to java proxy

--- api/routes/paypalRoutes.js
+++ api/routes/paypalRoutes.js
@@ -3,21 +3,21 @@ const { body } = require("express-validator");
 
 module.exports = function (app) {
   //Create an auth JWT based on valid token from penny bank.
-  app.post("/api/pennybankplus/auth", paypalControllers.auth);
+  app.post("/sub/api/pennybankplus/auth", paypalControllers.auth);
 
   //Create a new subscription and get id
-  app.post("/api/pennybankplus/create/subscription", paypalControllers.create);
+  app.post("/sub/api/pennybankplus/create/subscription", paypalControllers.create);
 
   //Check status of a subscription by id
   app.post(
-    "/api/pennybankplus/status",
+    "/sub/api/pennybankplus/status",
     body("subscriptionId").isLength({ min: 14, max: 14 }),
     paypalControllers.status
   );
 
   //Cancel a subscription by id
   app.post(
-    "/api/pennybankplus/cancel",
+    "/sub/api/pennybankplus/cancel",
     body("subscriptionId").isLength({ min: 14, max: 14 }),
     paypalControllers.cancel
   );
